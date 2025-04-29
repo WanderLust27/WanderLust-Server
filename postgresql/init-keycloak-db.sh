7967208de3bfec19679613c74e3fdec2d5cfb865
Kryptonite qt <endy.panda@outlook.com>
2025-04-29 12:33:33 +0100

    app , db , and keycloak services setup

--- postgresql/init-keycloak-db.sh
+++ postgresql/init-keycloak-db.sh
@@ -5,4 +5,10 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE USER keycloak_user WITH PASSWORD 'keycloak_pass';
     CREATE DATABASE keycloak_db;
     GRANT ALL PRIVILEGES ON DATABASE keycloak_db TO keycloak_user;
+
+    -- Connect to keycloak_db and grant permissions
+    \c keycloak_db
+    GRANT ALL ON SCHEMA public TO keycloak_user;
+    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO keycloak_user;
+    GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO keycloak_user;
 EOSQL
