5dd1463e48d6418b99d15c732d1edab5e177ce79
Kryptonite qt <endy.panda@outlook.com>
2025-05-03 16:03:08 +0100

    created abstarct user class

---
+++ src/main/java/com/wanderlust/wanderlust_app/user/User.java
@@ -0,0 +1,74 @@
+package com.wanderlust.wanderlust_app.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public abstract class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+    @Column(name = "PHONE_NUMBER")
+    private String phoneNumber;
+    @Column(name = "LAST_LOGIN")
+    private LocalDateTime lastLogin;
+    @Column(name = "CREATED_AT")
+    private LocalDateTime createdAt;
+    @Column(name = "UPDATED_AT")
+    private LocalDateTime updatedAt;
+    @Column(name = "DELETED_AT")
+    private LocalDateTime deletedAt;
+
+
+    @Override
+    public List<GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(  "ROLE_"+ getUserRole()));
+    }
+
+    public abstract String getUserRole();
+
+    public String getUsernameField(){
+        return username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
--- src/main/java/com/wanderlust/wanderlust_app/utils/security/JwtService.java
+++ src/main/java/com/wanderlust/wanderlust_app/utils/security/JwtService.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
-import org.youcode.EventLinkerAPI.user.User;
 
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
@@ -21,66 +20,5 @@ import java.util.stream.Collectors;
 
 @Component
 public class JwtService {
-    private final Key jwtSecretKey;
-    private final Long expirationTime;
 
-    public JwtService(@Value("${JWT_SECRET_KEY}") String secretKey,
-                      @Value("${JWT_EXPIRATION_TIME}") Long expirationTime) {
-        this.jwtSecretKey = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
-        this.expirationTime = expirationTime;
-    }
-    public String extractUsername(String token) {
-        return extractClaim(token, Claims::getSubject);
-    }
-
-    public Date extractExpiration(String token) {
-        return extractClaim(token, Claims::getExpiration);
-    }
-    public List<String> extractRole(String token) {
-        Claims claims = extractAllClaims(token);
-        return claims.get("role", List.class);
-    }
-
-    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
-        final Claims claims = extractAllClaims(token);
-        return claimsResolver.apply(claims);
-    }
-
-    private Claims extractAllClaims(String token) {
-        return Jwts.parserBuilder().setSigningKey(jwtSecretKey).build().parseClaimsJws(token).getBody();
-    }
-
-    private Boolean isTokenExpired(String token) {
-        return extractExpiration(token).before(new Date());
-    }
-
-    public String generateToken(UserDetails userDetails) {
-        User user = (User) userDetails;
-        return createToken(userDetails);
-    }
-
-
-    private List<String> getRoles(UserDetails userDetails) {
-        return userDetails.getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority)
-                .collect(Collectors.toList());
-    }
-
-    private String createToken(UserDetails userDetails) {
-        User user = (User) userDetails;
-
-        return Jwts.builder()
-                .claim("roles", getRoles(userDetails))
-                .claim("username", user.getUsernameField())
-                .setSubject(userDetails.getUsername())
-                .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
-                .signWith(jwtSecretKey, SignatureAlgorithm.HS256)
-                .compact();
-    }
-
-    public Boolean validateToken(String token, UserDetails userDetails) {
-        final String username = extractUsername(token);
-        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
-    }
 }
